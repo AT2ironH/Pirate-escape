919beb1b9e17cf30c57e8d36ceb653ed0b150621
Aleksandar <aleksandar.m.trifunovic@gmail.com>
2021-01-25 15:44:09 +0100

    randomised chain drop

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ let playerX = 360
 let playerY = 650
 let playerWidth = 60
 let playerHeight = 50
-let playerIncrementX = 1
-let playerDecrementY = 1
-let playerDecrementX = 1
-let playerIncrementY = 1
+let playerIncrementX = 0.5
+let playerDecrementY = 0.5
+let playerDecrementX = 0.5
+let playerIncrementY = 0.5
 let isLeftArrow = false;
 let isRightArrow = false;
 let isArrowUp = false;
@@ -19,14 +19,13 @@ let playerShipX = canvas.width / 4
 let playerShipY = canvas.height - playerShipHeight
 let pirateShipX = canvas.width / 6
 let pirateShipY = -80
-let chain = [{x:canvas.width /1.9 , y:-625 }]
-
-
-
+let coordinatesY = Math.floor(Math.random()*(playerShipY + playerShipHeight -playerShipY +1 )) + playerShipY
+let coordinatesX = Math.floor(Math.random()*(playerShipX + playerShipWidth -playerShipX +1 )) + playerShipX
+let chain = {x:coordinatesX , y:-200 }
 
 
  
-let startScreen = document.createElement(`img`)
+let startScreen = document.createElement(`img`)     //FINISHED
 startScreen.src = "/imgs/startScreen.png"
 
 
@@ -53,7 +52,7 @@ pirateImg.src = "/imgs/pirate.png"
 
 
 
-document.addEventListener(`keydown`, (event) => {
+document.addEventListener(`keydown`, (event) => {  //FINISHED
     if (event.key == `ArrowRight`){
         isRightArrow = true;
         isLeftArrow = false;
@@ -98,42 +97,47 @@ document.addEventListener(`keydown`, (event) => {
 function elements(){
     ctx.drawImage(backgroundImg, 0, 0)
     ctx.drawImage(playerShipImg, playerShipX, playerShipY, playerShipWidth, playerShipHeight )
-    ctx.drawImage(harpoonImg, canvas.width /1.9, -625)
-    for (let i = 0; i<chain.length; i++){
-        ctx.drawImage(harpoonImg, chain[i].x, chain[i].y) 
-        chain[i].y ++
-    
-        if(chain[i].y > canvas.height / 8){
-            chain[i] --
-                                                   
-        }
-}
+    // ctx.drawImage(harpoonImg, canvas.width /1.9, -625)
+    if (chain.y + harpoonImg.height > coordinatesY){
+        ctx.drawImage(harpoonImg, chain.x, chain.y)
+        // chain.y --
+        // here call the function that moves up
+       }
+    else {
+        ctx.drawImage(harpoonImg, chain.x, chain.y) 
+        chain.y ++
+    }
+
+
     
     ctx.drawImage(shipImg, pirateShipX, pirateShipY, shipImg.width *1.3, shipImg.height *1.3)
     ctx.drawImage(playerImg, playerX, playerY, playerWidth,playerHeight)
     ctx.drawImage(pirateImg, canvas.width /2 , 80)
 
-    if(isLeftArrow){
+    if(isLeftArrow && playerX  > playerShipX -15){  // FINISHED
         playerX -= playerDecrementX
     }
-    else if(isRightArrow){
+    else if(isRightArrow && playerWidth + playerX < canvas.width - playerShipX +10){
         playerX += playerIncrementX
     }
-    else if(isArrowUp){
+    else if(isArrowUp && playerY > playerShipY -15){
         playerY -= playerDecrementY
     }
-    else if (isArrowDown){
+    else if (isArrowDown && playerY + playerHeight < playerShipY + playerShipHeight -5 ){
         playerY += playerIncrementY
     }
 
 
-    
-
-    
-
 
 }
 
+function breakTheChain(){
+    if((playerX + playerWidth) && ( playerY + playerHeight) == coordinatesY){
+        chain.y = 0
+    }
+
+
+}
 
 
 
@@ -149,5 +153,5 @@ function game(){
 window.addEventListener('load', () => {
 intervalId = setInterval (() => {
     requestAnimationFrame(game)
-    }, 1)
+    }, 5)
 })
